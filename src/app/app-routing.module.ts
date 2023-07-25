89e15e2e86a0f7d6ac3370fc0bc5a0a516deff82
Ganesh-HA <haganesh2000@gmail.com>
2023-07-25 16:26:35 +0530

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+const routes: Routes = [{path:'hello', component:HelloComponent},{path:'', redirectTo:'/hello',pathMatch:'full'},
+{path:'User-profile', component:UserProfileComponent}];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
